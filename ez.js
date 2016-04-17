ac6be350a7540ca3f183d68cdc023ac107747296
Luke Shimkus <super99leo@gmail.com>
2016-04-17 11:24:33 -0400

    Update ez.js

--- ez.js
+++ ez.js
@@ -1,32 +1,37 @@
-Array.prototype.remove = function(object){
-  for(var i = 0; i < this.length; i++){
-    if(this[i] == object){
-      this.splice(i, 1);
-      return this;
+var Random = {
+    color: function() {
+        return "#" + ((1 << 24) * Math.random() | 0).toString(16);
     }
-  }
+};
+
+Array.prototype.remove = function(object) {
+    for (var i = 0; i < this.length; i++) {
+        if (this[i] == object) {
+            this.splice(i, 1);
+            return this;
+        }
+    }
 };
 
 Array.prototype.removeAll = function(what) {
-      for(var i = this.length; i--;) {
-          if(this[i] === what) {
-              this.splice(i, 1);
-          }
-      }
-  return this;
+    for (var i = this.length; i--;) {
+        if (this[i] === what) {
+            this.splice(i, 1);
+        }
+    }
+    return this;
 };
 
-Array.prototype.append = function(){
-  for(var i = 0; i < arguments.length; i++){
-    this.push(arguments[i]);
-  }
-  return this;
+Array.prototype.append = function() {
+    for (var i = 0; i < arguments.length; i++) {
+        this.push(arguments[i]);
+    }
+    return this;
 };
 
-Array.prototype.prepend = function(){
-  for(var i = 0; i < arguments.length; i++){
-    this.unshift(arguments[i]);
-  }
-  return this;
+Array.prototype.prepend = function() {
+    for (var i = 0; i < arguments.length; i++) {
+        this.unshift(arguments[i]);
+    }
+    return this;
 };
-
