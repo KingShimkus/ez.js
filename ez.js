945b25e94c5c27c75a10310335fb1c97b18f674b
Luke Shimkus <super99leo@gmail.com>
2016-04-17 11:21:30 -0400

    Update ez.js

--- ez.js
+++ ez.js
@@ -1,1 +1,32 @@
+Array.prototype.remove = function(object){
+  for(var i = 0; i < this.length; i++){
+    if(this[i] == object){
+      this.splice(i, 1);
+      return this;
+    }
+  }
+};
+
+Array.prototype.removeAll = function(what) {
+      for(var i = this.length; i--;) {
+          if(this[i] === what) {
+              this.splice(i, 1);
+          }
+      }
+  return this;
+};
+
+Array.prototype.append = function(){
+  for(var i = 0; i < arguments.length; i++){
+    this.push(arguments[i]);
+  }
+  return this;
+};
+
+Array.prototype.prepend = function(){
+  for(var i = 0; i < arguments.length; i++){
+    this.unshift(arguments[i]);
+  }
+  return this;
+};
 
